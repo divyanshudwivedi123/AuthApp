d424077a0c9c5c0edf4a2ea5dec272239bb22e6a
dd <dwivedidivyanshu30@gmail.com>
2024-06-03 12:05:14 +0530

    Created signup and signin controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const bcryptjs = require("bcryptjs");
+const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -14,4 +16,24 @@ const signup = async (req, res, next) => {
     
 }
 
-module.exports = signup;
+const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, "Wrong credentials"));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000)
+        res.cookie("access_token", token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+module.exports = {
+    signup,
+    signin
+}
