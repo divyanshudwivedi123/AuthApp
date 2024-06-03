4852d6afc20d7835c2b79093b1814955271f50f3
dd <dwivedidivyanshu30@gmail.com>
2024-06-04 01:44:29 +0530

    Added signout feature

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {signup, signin, google} = require("../controllers/auth.controller");
+const {signup, signin, google, signout} = require("../controllers/auth.controller");
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signout);
 
 
 module.exports = router;
