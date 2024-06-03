2d68af5c273d4c7dc21bdbf2531ec1ac499e4337
dd <dwivedidivyanshu30@gmail.com>
2024-06-04 02:24:43 +0530

    URL updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://authapp-u514.onrender.com',
+        target: 'https://authapp-btzm.onrender.com',
         secure: true,
       },
     },
